bc59348c411a1234a7c70aee30bcaa6cab12ff96
Kurogoma4D <highdro0479@gmail.com>
2020-10-25 10:27:01 +0900

    add: introduce GameState to control entirely game state

--- lib/app_state/app_state_controller.dart
+++ lib/app_state/app_state_controller.dart
@@ -1,9 +1,22 @@
+import 'dart:async';
 import 'dart:ui';
 import 'dart:math' as math;
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_snake/app_state/app_state.dart';
 import 'package:flutter_snake/game_board.dart';
+
+enum GameState {
+  initial,
+  running,
+  finished,
+}
+
+class GameStateNotifier extends StateNotifier<GameState> {
+  GameStateNotifier() : super(GameState.initial);
+}
+
+final gameStateProvider = StateNotifierProvider((_) => GameStateNotifier());
 
 final _random = math.Random();
 
@@ -20,12 +33,30 @@ final appStateControllerProvider = Provider((ref) => AppStateController(
           _random.nextInt(BOARD_SIZE).toDouble(),
         ),
       ),
+      ref: ref,
     ));
 
 class AppStateController {
   AppState state;
+  StreamSubscription clock;
 
-  AppStateController({this.state});
+  AppStateController({this.state, ProviderReference ref}) {
+    clock = ref.read(clockProvider.stream).listen((_) {
+      _updateGameState();
+    });
+
+    ref.read(gameStateProvider).stream.listen((state) {
+      switch (state) {
+        case GameState.initial:
+          break;
+        case GameState.running:
+          break;
+        case GameState.finished:
+          clock.cancel();
+          break;
+      }
+    });
+  }
 
   void setDirection(InputDirection value) {
     state = state.copyWith(currentDirection: value);
@@ -72,7 +103,7 @@ class AppStateController {
     );
   }
 
-  void updateGameState() {
+  void _updateGameState() {
     _updatePlayer();
 
     if (state.snake.first == state.item) {
